65a919ac90ff53ff51dafcc601fd7ec64cc07e99
mederitus <Jhgf@9510>
2021-08-23 19:48:55 -0300

    23082021

--- src/repository/FuncionarioRepository.java
+++ src/repository/FuncionarioRepository.java
@@ -1,4 +1,41 @@
 package repository;
 
+import util.ConnectionManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class FuncionarioRepository {
+
+    public static void findAll() throws SQLException, ClassNotFoundException {
+        Connection conn = ConnectionManager.getConnection();
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM funcionario ");
+        ResultSet resultSet = null;
+        resultSet = statement.executeQuery();
+
+        System.out.println("---- BUSCA POR ID INI ----");
+        while (resultSet.next()) {
+            String index = resultSet.getString(2);
+            System.out.println(index);
+        }
+        System.out.println("---- BUSCA POR ID FIM ----");
+        conn.close();
+    }
+
+    public static void findById(Integer id) throws SQLException, ClassNotFoundException {
+        Connection conn = ConnectionManager.getConnection();
+        PreparedStatement statement3 = conn.prepareStatement("SELECT * FROM funcionario WHERE id = ?");
+        statement3.setInt(1, id);
+        ResultSet resultSet3 = null;
+        resultSet3 = statement3.executeQuery();
+
+        System.out.println("---- Listagem INI ----");
+        while (resultSet3.next()) {
+            String index = resultSet3.getString(2);
+            System.out.println(index);
+        }
+        System.out.println("---- Listagem FIM ----");
+        conn.close();
+    }
 }

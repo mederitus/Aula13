b38e42ea74f3cd8de86e0d3954c94554810e85b6
mederitus <Jhgf@9510>
2021-08-23 19:37:03 -0300

    23082021

--- src/util/ConnectionManager.java
+++ src/util/ConnectionManager.java
@@ -6,11 +6,12 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    public static Connection getConnection() throws ClassNotFoundException, SQLException{
-
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         String url = "jdbc:postgresql://localhost:5432/postgres";
         Connection conn = DriverManager.getConnection(url, "postgres", "postgres");
-
+        Connection conn1 = conn;
+        return conn1;
     }
+
 }
